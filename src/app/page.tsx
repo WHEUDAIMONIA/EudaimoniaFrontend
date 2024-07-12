c0f0784048133d052b839db4c23c523ed52919ed
Rohit Kotha <rohitkotha10@gmail.com>
2024-07-12 16:28:54 +0530

    working with a backend sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 'use client'
 import Button from './components/Button'
 
-export default function Home() {
-  const callAPI = async () => {
-    try {
-      const res = await fetch(`https://api.sampleapis.com/coffee/hot`)
-      const data = await res.json()
-      console.log(data)
-    } catch (err) {
-      console.log(err)
-    }
+export async function POSTFlask() {
+  try {
+    const val1 = 'rohit'
+    const val2 = '2'
+    const params = new URLSearchParams({
+      name: val1,
+      age: val2
+    })
+    const url = `http://localhost:5000/fetchuser?${params.toString()}`
+    console.log(url)
+    const res = await fetch(url, {
+      method: 'GET'
+      // headers: {
+      //   'Content-Type': 'application/json'
+      //   // 'API-Key': process.env.DATA_API_KEY!
+      // },
+      // body: JSON.stringify({ name: 'Hello Rohit' })
+    })
+    const data = await res.json()
+    console.log(data)
+    return Response.json(data)
+  } catch (err) {
+    console.log(err)
   }
+}
 
+export default function Home() {
   return (
     <div>
       <section className='flex flex-col items-center justify-center py-24'>
@@ -23,7 +39,7 @@ export default function Home() {
         <div className='text-text-secondary my-6 px-20 text-center text-2xl'>
           {`Mental Health Made Easy ...`}
         </div>
-        <Button onClick={callAPI}>Hello</Button>
+        <Button onClick={POSTFlask}>Hello</Button>
       </section>
     </div>
   )
